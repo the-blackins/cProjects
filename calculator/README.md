b0ca1263d65102f6934afbf0059e6c296f89a35b
samuel <houseofblackins@gmail.com>
2024-10-04 10:57:43 +0100

    Did the basic operation of the simple calculator

---
+++ calculator/README.md
@@ -0,0 +1,5 @@
+simple calculator 
+given a list of possible math operations to the user
+accept two input arguments from the user which could either be a positive or negative integer or float
+then store it into two variables respectively 
+using a loop to iterate over 
---
+++ calculator/calculator.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double subtraction(double a, double b) {
+    return a - b;
+}
+
+double divide(double a, double b) {
+    return a / b;
+}
+
+int main() {
+    int commence;
+
+    printf("Begin operation [yes = 1 / no = 0]: ");
+    scanf("%d", &commence);
+ 
+    while (commence == 1) {
+        double num1, numN, result;
+        char oper;
+
+        printf("Input number: ");
+        scanf("%lf", &num1);
+
+        printf("Choose your operator: - + * / : ");
+        scanf(" %c", &oper);  
+        
+        printf("Input number: ");
+        scanf("%lf", &numN);
+        
+        switch (oper) {
+            case '+':
+                result = add(num1, numN);
+                printf("Result: %.2lf\n", result);
+                break;
+            case '-':
+                result = subtraction(num1, numN);
+                printf("Result: %.2lf\n", result);
+                break;
+            case '*':
+                result = multiply(num1, numN);
+                printf("Result: %.2lf\n", result);
+                break;
+            case '/':
+                if (numN == 0) {
+                    printf("Error: Division by zero is undefined.\n");
+                } else {
+                    result = divide(num1, numN);
+                    printf("Result: %.2lf\n", result);
+                }
+                break;
+            default:
+                printf("Error: Invalid operator. Please use +, -, *, or /.\n");
+                break;
+        }
+        
+        printf("Continue operation [yes = 1 / no = 0]: ");
+        scanf("%d", &commence);
+    }
+
+    printf("Exiting the operation. Goodbye!\n");
+    return 0;
